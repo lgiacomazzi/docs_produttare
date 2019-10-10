6689ea1d5a2a819fdbb0c2b4a8b750cea51b9972
Luciano Giacomazzi <lucianogiacomazzi@gmail.com>
2019-10-10 15:16:44 -0300

    popup edit js & css

---
+++ popup/popup_raise_hand.js
@@ -0,0 +1,36 @@
+const css = {
+  rel: "stylesheet",
+  href: "http://lgiacomazzi.github.com/produttare/popup/popup.css"
+};
+
+const div = {
+  id: "produttare_raise_hand",
+  href: "http://eventos.produttare.com.br/fale_com_consultor"
+};
+
+const objeto =
+  "<span class=`fa fa-headset fa-lg mr-3a></span><span>Fale com Consultor</span>";
+
+var head = document.getElementsByTagName("head")[0];
+const link = document.createElement("link");
+for (let [key, value] of Object.entries(css)) {
+  link.setAttribute(key, value);
+}
+head.appendChild(link);
+
+const popup = document.createElement("div");
+for (let [key, value] of Object.entries(div)) {
+  popup.setAttribute(key, value);
+}
+popup.innerHTML = objeto;
+body.appendChild(popup);
+
+//
+// <script src="https://lgiacomazzi.github.io/produttare/js_base_produttare.js"></script>
+// <a
+//   href="http://eventos.produttare.com.br/fale_com_consultor"
+//   id="produttare_raise_hand"
+// >
+//   <span class="fa fa-headset fa-lg mr-3"></span><span>Fale com Consultor</span>
+// </a>
+// <style></style>
